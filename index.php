64f174d8638c62a36246aa35174eeb01a2317aab
Kevin Schleuning <Schleuning@gmx.de>
2014-11-23 21:23:45 +0100

    Testdatei für GitHub

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css">
+<title>Online Fotoshop</title>
+</head>
+<body>
+<header>
+<nav id="nav_top"></nav>
+<section id="header_img"></section>
+<?php
+            echo 'T';
+?>
+
+<nav id="nav_main"></nav>
+</header>
+<main>Das ist ein Test.</main>
+<footer></footer>
+</body>
+</html>
